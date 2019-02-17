aeb06ded4508313fc0d15cd3d312635cb1c35aac
Chethan K <krishnamurthy.chethan@gmail.com>
2019-02-17 16:20:07 +0530

    Inversion of Control

--- src/com/missionjava/mycoach/Application.java
+++ src/com/missionjava/mycoach/Application.java
@@ -1,12 +1,22 @@
 package com.missionjava.mycoach;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Application {
 	
 	public static void main(String[] args) {
 		
-		Coach bCoach = new TrackCoach();
+		//load spring configuration file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
 		
+		//retrieve bean from spring container
+		Coach bCoach = context.getBean("myCoach", Coach.class);
+		
+		//call methods using bean
 		System.out.println(bCoach.getDailyWorkout());
+		
+		//close app context
+		context.close();
 	}
 
 }
