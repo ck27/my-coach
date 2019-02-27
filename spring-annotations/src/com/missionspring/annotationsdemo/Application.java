ffcee71277c4a7946ecd7e274ffc49d390e643cc
Chethan K <krishnamurthy.chethan@gmail.com>
2019-02-27 21:38:43 +0530

    Specifying scope using annotations

--- spring-annotations/src/com/missionspring/annotationsdemo/Application.java
+++ spring-annotations/src/com/missionspring/annotationsdemo/Application.java
@@ -16,6 +16,14 @@ public class Application {
 		System.out.println(aCricketCoach.getDailyWorkout());
 		System.out.println(aCricketCoach.getDailyFortune());
 		
+		Coach rcbCoach = context.getBean("cricketCoach", Coach.class);
+		
+		boolean sameCricketBeans = aCricketCoach == rcbCoach;
+		System.out.println("fCoach and manuCoach are the same " + sameCricketBeans);
+		System.out.println("fCoach memory location " + aCricketCoach);
+		System.out.println("manuCoach memory location " + rcbCoach);
+		
+		
 		Coach trackCoach = context.getBean("trackCoach", Coach.class);
 		System.out.println(trackCoach.getDailyWorkout());
 		System.out.println(trackCoach.getDailyFortune());
@@ -23,6 +31,13 @@ public class Application {
 		Coach fCoach = context.getBean("footballCoach", Coach.class);
 		System.out.println(fCoach.getDailyWorkout());
 		System.out.println(fCoach.getDailyFortune());
+		
+		Coach manuCoach = context.getBean("footballCoach", Coach.class);
+		
+		boolean sameBeans = fCoach == manuCoach;
+		System.out.println("fCoach and manuCoach are the same " + sameBeans);
+		System.out.println("fCoach memory location " + fCoach);
+		System.out.println("manuCoach memory location " + manuCoach);
 		
 		context.close();
 	}
