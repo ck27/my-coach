a93a93a13b23ca6b1bdea052e5f9dd9b8a759f7c
Chethan K <krishnamurthy.chethan@gmail.com>
2019-02-27 21:41:32 +0530

    Specifying lifecycle hooks using annotaions

--- spring-annotations/src/com/missionspring/annotationsdemo/FootballCoach.java
+++ spring-annotations/src/com/missionspring/annotationsdemo/FootballCoach.java
@@ -1,5 +1,8 @@
 package com.missionspring.annotationsdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -10,6 +13,11 @@ public class FootballCoach implements Coach {
 	@Autowired
 	@Qualifier("restFortuneService")
 	private FortuneService fortuneService;
+	
+	@PostConstruct
+	public void doPostStartUpStuff() {
+		System.out.println(">> doPostStartUpStuff()");
+	}
 
 	@Override
 	public String getDailyWorkout() {
@@ -19,6 +27,11 @@ public class FootballCoach implements Coach {
 	@Override
 	public String getDailyFortune() {
 		return this.fortuneService.getFortune();
+	}
+	
+	@PreDestroy
+	public void doPreDestroyStuff() {
+		System.out.println(">> doPreDestroyStuff()");
 	}
 
 }
