ea9a7b973e0c58a8b641d5f6a40a0c8933c50011
Chethan K <krishnamurthy.chethan@gmail.com>
2019-03-31 21:54:56 +0530

    Injecting Values from Properties file

--- spring-config/src/com/missionspring/mycoach/AppConfig.java
+++ spring-config/src/com/missionspring/mycoach/AppConfig.java
@@ -2,8 +2,10 @@ package com.missionspring.mycoach;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("sports.properties")
 public class AppConfig {
 
 	
--- spring-config/src/com/missionspring/mycoach/SwimCoach.java
+++ spring-config/src/com/missionspring/mycoach/SwimCoach.java
@@ -1,21 +1,29 @@
 package com.missionspring.mycoach;
 
-public class SwimCoach implements Coach{
-	
+import org.springframework.beans.factory.annotation.Value;
+
+public class SwimCoach implements Coach {
+
+	@Value("${foo.email}")
+	private String email;
+
+	@Value("${foo.team}")
+	private String team;
+
 	private FortuneService fortuneService;
-	
+
 	public SwimCoach(FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
 	}
 
 	@Override
 	public String getDailyWorkout() {
-		return "Warm up swim for 1000m";
+		return this.team + " : Warm up swim for 1000m";
 	}
 
 	@Override
 	public String getDailyFortune() {
-		return this.fortuneService.getFortune();
+		return this.email + " : " + this.fortuneService.getFortune();
 	}
 
 }
