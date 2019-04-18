578b7737d988113ea3dae53406a431401746ebd4
Chethan K <krishnamurthy.chethan@gmail.com>
2019-04-18 14:38:06 +0530

    Custom Validation messages and Regular Expressions

--- spring-mvc-demo/src/com/missionspring/mvcdemo/Customer.java
+++ spring-mvc-demo/src/com/missionspring/mvcdemo/Customer.java
@@ -3,6 +3,7 @@ package com.missionspring.mvcdemo;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Customer {
@@ -15,9 +16,13 @@ public class Customer {
 	private String lastName;
 
 	
+	@NotNull(message="is required")
 	@Min(value=0, message="value should not be less than 0")
 	@Max(value=10, message="value should not be greater than 10")
-	private int freePasses;
+	private Integer freePasses;
+	
+	@Pattern(regexp="^[0-9a-zA-Z]{5}", message = "Postal code should contain only alhpabets or numbers of length 5")
+	private String postalCode;
 	
 	public String getFirstName() {
 		return firstName;
@@ -35,13 +40,21 @@ public class Customer {
 		this.lastName = lastName;
 	}
 
-	public int getFreePasses() {
+	public Integer getFreePasses() {
 		System.out.println("GET Free Passes " + freePasses);
 		return freePasses;
 	}
 
-	public void setFreePasses(int freePasses) {
+	public void setFreePasses(Integer freePasses) {
 		System.out.println("SET Free Passes " + freePasses);
 		this.freePasses = freePasses;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
 	}
 }
